3df4679fa25622de3241170d05afa8f12792d51e
Alexander Feiglstorfer <delooks@gmail.com>
2017-06-13 14:08:47 +0200

    init

---
+++ config.js
@@ -0,0 +1,15 @@
+// themeId: The space id which you can find on storyblok app.storyblok.com in the space settings.
+// domain: The domain without the protocol. Example: city.me.storyblok.com
+
+module.exports = {
+  blok: {
+    apiVersion: 2,
+    themeId: '40479',
+    domain: 'fc147f74.me.storyblok.com',
+    apiKey: require('./token'),
+    basePath: 'views',
+    quickstartToken: '',
+    environment: 'live',
+    enableQuickstartTour: true
+  }
+}
