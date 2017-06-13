987b42b32ba530a93d53609d94d17f90a7df7ce8
Dominik Angerer <dominikangerer1@gmail.com>
2017-06-13 17:09:55 +0200

    tracks current step after api requests are done.

--- source/js/scripts.js
+++ source/js/scripts.js
@@ -1,3 +1,4 @@
+// pretty print on load
 window.onload = () => {
   var toPrettyPrint = document.querySelectorAll('[data-pretty]')
   for (var index = 0, max = toPrettyPrint.length; index < max; index++) {
@@ -10,17 +11,62 @@ window.onload = () => {
 
     element.innerHTML = JSON.stringify(clearStory(json), null, 2)
   }
+
+  var activeApiSteps = JSON.parse(window.localStorage.getItem('activeApiSteps'))
+  if (!activeApiSteps) {
+    activeApiSteps = []
+    window.localStorage.setItem('activeApiSteps', JSON.stringify(activeApiSteps))
+  }
+
+  for (var index = 0, max = activeApiSteps.length; index < max; index++) {
+    var activeStep = activeApiSteps[index];
+
+    var codeBlock = document.querySelector(activeStep)
+    codeBlock.classList.remove('u-hidden')
+
+    var requestButton = document.querySelector(`[data-show="${activeStep}"]`)
+    if (requestButton) {
+      requestButton.classList.add('u-hidden')
+    }
+
+    var actualStep = findAncestor(codeBlock, 'step')
+    actualStep.classList.add('step--active')
+  }
 }
 
+// show api requests
 var requestButtons = document.querySelectorAll('[data-show]')
 for (var index = 0, max = requestButtons.length; index < max; index++) {
   var element = requestButtons[index];
+
   element.addEventListener('click', (event) => {
+    var toShowId = event.currentTarget.getAttribute('data-show')
     var toShow = document.querySelector(event.currentTarget.getAttribute('data-show'))
+
+    event.currentTarget.classList.add('u-hidden')
     toShow.classList.remove('u-hidden')
+
+    var stepHistory = JSON.parse(window.localStorage.getItem('activeApiSteps'))
+
+    if (stepHistory.indexOf(toShowId) == -1) {
+      stepHistory.push(toShowId)
+      window.localStorage.setItem('activeApiSteps', JSON.stringify(stepHistory))
+    }
+
+    findAncestor(event.currentTarget, 'step').classList.add('step--active')
+
   })
 }
 
+
+
+// find nearest element with class
+function findAncestor(el, cls) {
+  while ((el = el.parentElement) && !el.classList.contains(cls));
+  return el;
+}
+
+// clear story data for simplicity
 function clearStory(story) {
   // provide clear data
   delete story.alternates
